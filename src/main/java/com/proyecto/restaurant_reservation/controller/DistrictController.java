04e32e9fa9c289b7d4cf817e238e4e2251cef9c6
Dafne Cuvas <jesak1605@gmail.com>
2025-01-13 18:58:11 -0500

    feat: creacion de controller

--- src/main/java/com/proyecto/restaurant_reservation/controller/DistrictController.java
+++ src/main/java/com/proyecto/restaurant_reservation/controller/DistrictController.java
@@ -1,4 +1,25 @@
 package com.proyecto.restaurant_reservation.controller;
 
-public class DisctrictController {
+import com.proyecto.restaurant_reservation.dto.response.DistrictResponseDTO;
+import com.proyecto.restaurant_reservation.service.DistrictService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/districts")
+public class DistrictController {
+    private final DistrictService districtService;
+
+    @GetMapping
+    public ResponseEntity<List<DistrictResponseDTO>> getAllDistricts() {
+        List<DistrictResponseDTO> districts= districtService.getAllDistricts();
+        return ResponseEntity.ok(districts);
+    }
+
 }
--- src/main/java/com/proyecto/restaurant_reservation/controller/RestaurantController.java
+++ src/main/java/com/proyecto/restaurant_reservation/controller/RestaurantController.java
@@ -1,4 +1,42 @@
 package com.proyecto.restaurant_reservation.controller;
 
+import com.proyecto.restaurant_reservation.domain.entity.Restaurant;
+import com.proyecto.restaurant_reservation.dto.response.RestaurantResponseDTO;
+import com.proyecto.restaurant_reservation.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/restaurants")
 public class RestaurantController {
+    private final RestaurantService restaurantService;
+
+    //Recupera una lista paginada de todos los restaurantes.
+    @GetMapping("/page")
+    public ResponseEntity<Page<RestaurantResponseDTO>> getAllRestaurants(
+            @PageableDefault(size = 5) Pageable pageable){
+        Page<RestaurantResponseDTO> restaurants = restaurantService.getAllRestaurants(pageable);
+        return ResponseEntity.ok(restaurants);
+    }
+
+    //Busca restaurantes por distrito, devolviendo una lista paginada.
+    @GetMapping("/page/district")
+    public ResponseEntity<Page<RestaurantResponseDTO>> findByDistrictName(
+            @RequestParam String districtName,
+            @PageableDefault(sort = "name", size = 5) Pageable pageable){
+        Page<RestaurantResponseDTO> restaurants = restaurantService.getRestaurantsByDistrictName( pageable, districtName);
+        return ResponseEntity.ok(restaurants);
+    }
+
+    //Busca un restaurante específico por su ID.
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantResponseDTO> getRestaurantById(@PathVariable Long id){
+        RestaurantResponseDTO restaurant = restaurantService.getRestaurantById(id);
+        return ResponseEntity.ok(restaurant);
+    }
 }
